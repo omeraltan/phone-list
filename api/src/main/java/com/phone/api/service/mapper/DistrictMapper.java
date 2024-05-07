c4359f4cc030ae0af11c912d85b807889eb58113
omeraltan <omeraltan66@gmail.com>
2024-05-07 10:05:36 +0300

    Commit All

--- api/src/main/java/com/phone/api/service/mapper/DistrictMapper.java
+++ api/src/main/java/com/phone/api/service/mapper/DistrictMapper.java
@@ -7,5 +7,5 @@ import org.mapstruct.*;
 /**
  * Mapper for the entity {@link District} and its DTO {@link DistrictDTO}.
  */
-@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+@Mapper(componentModel = "spring")
 public interface DistrictMapper extends EntityMapper<DistrictDTO, District>{}
