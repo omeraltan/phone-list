aa6dcf3b1eb446f25a01b523861acae8b605bf84
omeraltan <omeraltan66@gmail.com>
2024-05-24 16:12:37 +0300

    Commit All

--- api/src/main/java/com/phone/api/repository/DistrictRepository.java
+++ api/src/main/java/com/phone/api/repository/DistrictRepository.java
@@ -1,8 +1,6 @@
 package com.phone.api.repository;
 
 import com.phone.api.domain.District;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
